b93a2e9e3c6c5718c314985043b1433fdfcaf7f4
Vinicius <vinicius0809@gmail.com>
2017-11-26 14:46:51 -0200

    Arquivo .arff gerado

--- arffwrite.m
+++ arffwrite.m
@@ -1,4 +1,4 @@
-function arffwrite(fname,data)
+function arffwrite(fname,data,classe)
 % fname This is file name without extension
 %data is m x n where m are the instances and n-1 are the features. The last
 %column is the class as integer
@@ -12,17 +12,8 @@ for jj=1:sss
 fprintf (out1, '@attribute %s numeric\n',num2str(jj));
 end
 n_classes=max(unique(data(:,end)));
-txt1=strcat('@attribute',{' '},num2str(sss+1),{' {'});
-
-for ii=1:n_classes
-txt1=strcat(txt1,num2str(ii),{','});
-end
-txt1=strcat(txt1,{'}'});
+txt1=strcat('@attribute',{' '},num2str(sss+1),{' {'},classe,{'}'});
 
 fprintf (out1, '%s\n\n',char(txt1));
 fprintf (out1,'@data\n');
-fprintf(out1, '\n');
 
-fclose(out1);
-
-dlmwrite (filename1, data, '-append' );
--- teste.m
+++ teste.m
@@ -1,19 +1,27 @@
-clc;close all;clear all;
+clc;
+close all;
+clear all;
 
 
 %Lendo o audio para obter o y
 [y,Fs] = audioread('testeg1.wav');
 
-%Tirando o silêncio do audio
+%Tirando o silï¿½ncio do audio
 %novoSinal = RemoveSilence(y, Fs);
 
+[y,Fs] = audioread('audioGeovanne2\1.wav') ;
+   T = wpdec(y,8,'db9');
+   E = wenergy(T);
+
+fileName = 'teste';
+classe = 'Geovanne';
+arffwrite(fileName, E,classe)
 
 for n = 1:63
    [y,Fs] = audioread(strcat('audioGeovanne2\', int2str(n), '.wav')) ;
    T = wpdec(y,8,'db9');
    E = wenergy(T);
-   arffwrite('teste', E)
-   
+   dlmwrite(strcat(fileName,'.arff'),E,'-append') 
 end
 fprintf('saiu');
 
